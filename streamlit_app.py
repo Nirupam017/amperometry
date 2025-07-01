c5504f4c7711080958eed3521593dd487d0a92b7
Nirupam017 <125028904+Nirupam017@users.noreply.github.com>
2025-07-01 10:11:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 st.markdown("""
     <style>
     .stApp {
-        background-image: url("https://wallpapercave.com/w/wp1936436");
+        background-image: url("https://wallpapercave.com/w/wp1936436.jpg");
         background-size: cover;
         background-position: center;
         color: white;
